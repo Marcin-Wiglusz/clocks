85f68edfb34231e5a86d57071779d1a717e35fdd
Marcin-Wiglusz <marcin.m.wiglusz@gmail.com>
2018-10-23 17:32:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -83,65 +83,6 @@ class Clock {
     // avoiding hands from rotating back to starting position when 90deg is reached
     (secDeg === 90) ? secHand.style.transition = '0s' : secHand.style.transition = '0.2s cubic-bezier(0.4, 2.08, 0.55, 0.44)';
     (minDeg === 90) ? minHand.style.transition = '0s' : minHand.style.transition = '0.2s cubic-bezier(0.4, 2.08, 0.55, 0.44)';
-
-
-
-
-
-    // need to make one loop for all hands and put bellow to seperate function
-    // doesn't work properly at the moment - hour hand glitches
-
-    // const secShadow = document.querySelectorAll('.second-hand');
-    // const minShadow = document.querySelectorAll('.min-hand');
-    // const hrsShadow = document.querySelectorAll('.hour-hand');
-    //
-    //
-    //
-    // for (var i = 0; i < secShadow.length; i++) {
-    //   // console.log(secShadow);
-    //
-    //   if (secDeg >= 300 && secDeg <= 450 || secDeg >= 0 && secDeg < 120) {
-    //     secShadow[i].classList.add('bottom-shadow');
-    //     secShadow[i].classList.remove('upper-shadow');
-    //   }
-    //   else {
-    //     secShadow[i].classList.add('upper-shadow');
-    //     secShadow[i].classList.remove('bottom-shadow');
-    //   }
-    // }
-    //
-    // for (var i = 0; i < minShadow.length; i++) {
-    //   // console.log(minShadow);
-    //
-    //   if (minDeg >= 300 && minDeg <= 450 || minDeg >= 0 && minDeg < 120) {
-    //     minShadow[i].classList.add('bottom-shadow');
-    //     minShadow[i].classList.remove('upper-shadow');
-    //   }
-    //   else {
-    //     minShadow[i].classList.add('upper-shadow');
-    //     minShadow[i].classList.remove('bottom-shadow');
-    //   }
-    // }
-    // // console.log(secDeg);
-    // // console.log(hrsDeg + " " + this.city);
-    // // console.log(this.city);
-    // // console.log(hrsDeg);
-    // // console.log(hrsShadow[0].classList);
-    // //
-    // // console.log(hrsShadow);
-    // for (var i = 0; i < hrsShadow.length; i++) {
-    //   console.log(hrsShadow[i]);
-    //   // console.log(this.city);
-    //   // for time over LA(-270) 1st condition has to be extended more below -180
-    //   if (hrsDeg >= -180 && hrsDeg < -60 || hrsDeg >= 120 && hrsDeg <= 300 || hrsDeg >= 480 && hrsDeg <= 660) {
-    //     hrsShadow[i].classList.add('upper-shadow');
-    //     hrsShadow[i].classList.remove('bottom-shadow');
-    //   }
-    //   else {
-    //     hrsShadow[i].classList.add('bottom-shadow');
-    //     hrsShadow[i].classList.remove('upper-shadow');
-    //   }
-    // }
   }
 }
 
